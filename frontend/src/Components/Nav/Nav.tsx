36a330fe331614c56353f7fc87d00db3814bc238
choitoady <healutoday@naver.com>
2024-04-01 16:50:41 +0900

    Feat : navbar 수정

--- frontend/src/Components/Nav/Nav.tsx
+++ frontend/src/Components/Nav/Nav.tsx
@@ -1,21 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { FiSearch } from "react-icons/fi";
+import { RxCross1 } from "react-icons/rx";
 import { SlMenu } from "react-icons/sl";
 import "./Nav.css";
 
-function Nav() {
+function Nav(): JSX.Element {
+  const [showSearch, setShowSearch] = useState<boolean>(false);
+
+  const toggleSearch = (): void => {
+    setShowSearch(!showSearch);
+  };
+
   return (
 
     <div className="Nav-container">
       <div className="header-box">
-        <div className="Logo">LOGO</div>
+        <div className="Logo">LANDING</div>
         <div className="navbar-button-box">
-          <button className="search-button">
-            <FiSearch size={30} />
-          </button>{" "}
-          <button onClick={() => console.log("버튼")} className="menu-button">
-            <SlMenu size={30} />
-          </button>
+          {showSearch && (
+            <form className="search-screen">
+              <label>
+                <input
+                  className="searchInput"
+                  type="text"
+                  placeholder="어떤 모임을 찾으시나요?"
+                />
+                <button type="submit" className="inScreen-search">
+                  <FiSearch />
+                </button>
+              </label>
+            </form>
+          )}
+          <div className="icon-container">
+            <button className="search-button" onClick={toggleSearch}>
+              {showSearch ? <RxCross1 size={27} /> : <FiSearch size={27} />}
+            </button>
+            <button className="menu-button">
+              <SlMenu size={27} />
+            </button>
+          </div>
         </div>
       </div>
     </div>
